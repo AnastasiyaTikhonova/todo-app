91b093203e201d79e407001b242f5ce38eed4691
Анастасия Тихонова <tikhonova-ai@nfk.local>
2023-11-30 17:14:59 +0300

    Work with modal window

--- src/containers/Tasks/Tasks.jsx
+++ src/containers/Tasks/Tasks.jsx
@@ -21,22 +21,26 @@ const Tasks = () => {
             </div>
             <Footer/>
             <Modal active={modalActive} setActive={setModalActive}>
-                <form className='form'>
                     <label htmlFor="title">Заголовок</label>
-                    <input id="title"/>
+                    <input id="title" type="text"/>
                     <label htmlFor="description">Описание</label>
-                    <textarea id="description"/>
-
-                    <p>Приоритет</p>
-
-
+                    <textarea id="description" type="text"/>
                     <input id="priority" type="checkbox" value="yes"/>
                     <label id="priorityLabel">Важно</label>
+                    <label>Файл к задаче</label>
+                    <input type="file" accept="image/*,video/*"/>
+                    <button id="addFileButton">Добавить</button>
+                    <label className="currStatus">Текущий статус</label>
+                    <select id="taskStatus" name="taskStatus">
+                        <option value="Queue">Queue</option>
+                        <option value="Development">Development</option>
+                        <option value="Done">Done</option>
+                    </select>
+                    <button id="addSubTaskButton">Добавить подзадачу</button>
+                <label htmlFor="comments">Комментарий</label>
+                <textarea id="comments" type="text"/>
 
-
-
-
-                </form>
+                <button className="addTuskButton">Добавить</button>
             </Modal>
         </div>
     )
